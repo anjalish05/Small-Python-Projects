cef753a22838aefcb0de33e2fdba16d4c414d654
anjalish05 <singhanjaliajab115@gmail.com>
2022-05-28 02:01:47 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,8 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter second number: ")
+
+add_result = float(num1) + float(num2)
+sub_result = float(num1) - float(num2)
+
+print(add_result)
+print(sub_result)
